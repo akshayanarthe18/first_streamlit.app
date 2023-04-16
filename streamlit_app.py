f3d714fe56ee9111fc1cb7936074fee4f7bdd036
akshayanarthe18 <130894247+akshayanarthe18@users.noreply.github.com>
2023-04-16 16:04:23 +0530

    Update streamlit_app.py
    added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and & Blueberry Hotmeal')
 streamlit.text('Kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
